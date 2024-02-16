2285686960b33eae9dd3151dce57746e088dad00
hisku <hiskias.melke1@gmail.com>
2024-02-16 14:59:00 +0900

    Remove buy buttons from EthOverview component

--- ui/components/app/wallet-overview/eth-overview.js
+++ ui/components/app/wallet-overview/eth-overview.js
@@ -33,10 +33,8 @@ import {
   getCurrentChainId,
   getPreferences,
   ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-  getSwapsDefaultToken,
   // getCurrentKeyring,
   // getIsBridgeChain,
-  getIsBuyableChain,
   // getMetaMetricsId,
   ///: END:ONLY_INCLUDE_IF
 } from '../../../selectors';
@@ -59,7 +57,6 @@ import { AssetType } from '../../../../shared/constants/transaction';
 import { Icon, IconName } from '../../component-library';
 import { IconColor } from '../../../helpers/constants/design-system';
 ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-import useRamps from '../../../hooks/experiences/useRamps';
 // import { getPortfolioUrl } from '../../../helpers/utils/portfolio';
 ///: END:ONLY_INCLUDE_IF
 import { useIsOriginalNativeTokenSymbol } from '../../../hooks/useIsOriginalNativeTokenSymbol';
@@ -75,11 +72,9 @@ const EthOverview = ({ className, showAddress }) => {
   ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
   // const location = useLocation();
   // const isBridgeChain = useSelector(getIsBridgeChain);
-  const isBuyableChain = useSelector(getIsBuyableChain);
   // const metaMetricsId = useSelector(getMetaMetricsId);
   // const keyring = useSelector(getCurrentKeyring);
   // const usingHardwareWallet = isHardwareKeyring(keyring?.type);
-  const defaultSwapsToken = useSelector(getSwapsDefaultToken);
   ///: END:ONLY_INCLUDE_IF
   const balanceIsCached = useSelector(isBalanceCached);
   const showFiat = useSelector(getShouldShowFiat);
@@ -94,62 +89,6 @@ const EthOverview = ({ className, showAddress }) => {
 
   const balance = useSelector(getSelectedAccountCachedBalance);
   // const isSwapsChain = useSelector(getIsSwapsChain);
-
-  ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-  const mmiPortfolioEnabled = useSelector(getMmiPortfolioEnabled);
-  const mmiPortfolioUrl = useSelector(getMmiPortfolioUrl);
-
-  const portfolioEvent = () => {
-    trackEvent({
-      category: MetaMetricsEventCategory.Navigation,
-      event: MetaMetricsEventName.MMIPortfolioButtonClicked,
-    });
-  };
-
-  const stakingEvent = () => {
-    trackEvent({
-      category: MetaMetricsEventCategory.Navigation,
-      event: MetaMetricsEventName.MMIPortfolioButtonClicked,
-    });
-  };
-
-  const renderInstitutionalButtons = () => {
-    return (
-      <>
-        <IconButton
-          className="eth-overview__button"
-          Icon={<Icon name={IconName.Stake} color={IconColor.primaryInverse} />}
-          label={t('stake')}
-          onClick={() => {
-            stakingEvent();
-            global.platform.openTab({
-              url: `${mmiPortfolioUrl}/stake`,
-            });
-          }}
-        />
-        {mmiPortfolioEnabled && (
-          <IconButton
-            className="eth-overview__button"
-            Icon={
-              <Icon name={IconName.Diagram} color={IconColor.primaryInverse} />
-            }
-            label={t('portfolio')}
-            onClick={() => {
-              portfolioEvent();
-              global.platform.openTab({
-                url: mmiPortfolioUrl,
-              });
-            }}
-          />
-        )}
-      </>
-    );
-  };
-  ///: END:ONLY_INCLUDE_IF
-
-  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-  const { openBuyCryptoInPdapp } = useRamps();
-  ///: END:ONLY_INCLUDE_IF
 
   return (
     <WalletOverview
@@ -209,42 +148,6 @@ const EthOverview = ({ className, showAddress }) => {
       }
       buttons={
         <>
-          {
-            ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-            <IconButton
-              className="eth-overview__button"
-              Icon={
-                <Icon
-                  name={IconName.PlusMinus}
-                  color={IconColor.primaryInverse}
-                />
-              }
-              disabled={!isBuyableChain}
-              data-testid="eth-overview-buy"
-              label={t('buyAndSell')}
-              onClick={() => {
-                openBuyCryptoInPdapp();
-                trackEvent({
-                  event: MetaMetricsEventName.NavBuyButtonClicked,
-                  category: MetaMetricsEventCategory.Navigation,
-                  properties: {
-                    location: 'Home',
-                    text: 'Buy',
-                    chain_id: chainId,
-                    token_symbol: defaultSwapsToken,
-                  },
-                });
-              }}
-            />
-            ///: END:ONLY_INCLUDE_IF
-          }
-
-          {
-            ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-            renderInstitutionalButtons()
-            ///: END:ONLY_INCLUDE_IF
-          }
-
           <IconButton
             className="eth-overview__button"
             data-testid="eth-overview-send"
@@ -273,140 +176,6 @@ const EthOverview = ({ className, showAddress }) => {
               });
             }}
           />
-          {/* <IconButton
-            className="eth-overview__button"
-            disabled={!isSwapsChain}
-            Icon={
-              <Icon
-                name={IconName.SwapHorizontal}
-                color={IconColor.primaryInverse}
-              />
-            }
-            onClick={() => {
-              ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-              global.platform.openTab({
-                url: `${mmiPortfolioUrl}/swap`,
-              });
-              ///: END:ONLY_INCLUDE_IF
-
-              ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-              if (isSwapsChain) {
-                trackEvent({
-                  event: MetaMetricsEventName.NavSwapButtonClicked,
-                  category: MetaMetricsEventCategory.Swaps,
-                  properties: {
-                    token_symbol: 'ETH',
-                    location: MetaMetricsSwapsEventSource.MainView,
-                    text: 'Swap',
-                    chain_id: chainId,
-                  },
-                });
-                dispatch(setSwapsFromToken(defaultSwapsToken));
-                if (usingHardwareWallet) {
-                  global.platform.openExtensionInBrowser(BUILD_QUOTE_ROUTE);
-                } else {
-                  history.push(BUILD_QUOTE_ROUTE);
-                }
-              }
-              ///: END:ONLY_INCLUDE_IF
-            }}
-            label={t('swap')}
-            data-testid="token-overview-button-swap"
-            tooltipRender={
-              isSwapsChain
-                ? null
-                : (contents) => (
-                    <Tooltip
-                      title={t('currentlyUnavailable')}
-                      position="bottom"
-                    >
-                      {contents}
-                    </Tooltip>
-                  )
-            }
-          /> */}
-          {/* {
-            ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-            <IconButton
-              className="eth-overview__button"
-              disabled={!isBridgeChain}
-              data-testid="eth-overview-bridge"
-              Icon={
-                <Icon name={IconName.Bridge} color={IconColor.primaryInverse} />
-              }
-              label={t('bridge')}
-              onClick={() => {
-                if (isBridgeChain) {
-                  const portfolioUrl = getPortfolioUrl(
-                    'bridge',
-                    'ext_bridge_button',
-                    metaMetricsId,
-                  );
-                  global.platform.openTab({
-                    url: `${portfolioUrl}${
-                      location.pathname.includes('asset') ? '&token=native' : ''
-                    }`,
-                  });
-                  trackEvent({
-                    category: MetaMetricsEventCategory.Navigation,
-                    event: MetaMetricsEventName.BridgeLinkClicked,
-                    properties: {
-                      location: 'Home',
-                      text: 'Bridge',
-                      chain_id: chainId,
-                      token_symbol: 'ETH',
-                    },
-                  });
-                }
-              }}
-              tooltipRender={
-                isBridgeChain
-                  ? null
-                  : (contents) => (
-                      <Tooltip
-                        title={t('currentlyUnavailable')}
-                        position="bottom"
-                      >
-                        {contents}
-                      </Tooltip>
-                    )
-              }
-            />
-            ///: END:ONLY_INCLUDE_IF
-          } */}
-          {/* {
-            ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-            <IconButton
-              className="eth-overview__button"
-              data-testid="eth-overview-portfolio"
-              Icon={
-                <Icon
-                  name={IconName.Diagram}
-                  color={IconColor.primaryInverse}
-                />
-              }
-              label={t('portfolio')}
-              onClick={() => {
-                const url = getPortfolioUrl(
-                  '',
-                  'ext_portfolio_button',
-                  metaMetricsId,
-                );
-                global.platform.openTab({ url });
-                trackEvent({
-                  category: MetaMetricsEventCategory.Navigation,
-                  event: MetaMetricsEventName.PortfolioLinkClicked,
-                  properties: {
-                    location: 'Home',
-                    text: 'Portfolio',
-                    chain_id: chainId,
-                    token_symbol: 'ETH',
-                  },
-                });
-              }}
-            />
-            ///: END:ONLY_INCLUDE_IF
-          } */}
         </>
       }
       className={className}
